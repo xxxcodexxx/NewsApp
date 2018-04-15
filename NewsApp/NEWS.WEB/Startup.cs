88eaa9aa734ea5c441cbf9cedc2f53c423f51864
NguyenThanhDon <nhockimi96tn@gmail.com>
2018-04-15 18:20:22 +0700

    init project webapi
    init project webapi

---
+++ NewsApp/NEWS.WEB/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NEWS.WEB.Startup))]
+
+namespace NEWS.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
