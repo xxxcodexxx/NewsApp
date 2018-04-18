f08945d3bbcd2f564aadb7a2ed3a35d4466b1d5b
Đăng Quang <tdquang1196@hotmail.com>
2018-04-18 23:04:22 +0700

    Revert "fix main-header & add category service"
    This reverts commit cf9ede3d3ea4389a985e43d6b7335b7e765b68ff.

--- NewsApp/NEWS.WEB/App/Service/category.service.ts
+++ NewsApp/NEWS.WEB/App/Service/category.service.ts
@@ -15,32 +15,6 @@ export class CategoryService {
             .catch(this.handleError);
     }
 
-    post(url: string, model: any): Observable<any> {
-        let body = JSON.stringify(model);
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-        return this._http.post(url, body, options)
-            .map((response: Response) => <any>response.json())
-            .catch(this.handleError);
-    }
-
-    put(url: string, id: number, model: any): Observable<any> {
-        let body = JSON.stringify(model);
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-        return this._http.put(url + id, body, options)
-            .map((response: Response) => <any>response.json())
-            .catch(this.handleError);
-    }
-
-    delete(url: string, id: number): Observable<any> {
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-        return this._http.delete(url + id, options)
-            .map((response: Response) => <any>response.json())
-            .catch(this.handleError);
-    }
-
     private handleError(error: Response) {
         console.error(error);
         return Observable.throw(error.json().error || 'Server error');
