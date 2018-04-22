c56694a7321cbdb1a18a85f03b8c3000892e2c87
xuanhung3296 <xuanhung3296@gmail.com>
2018-04-22 21:30:12 +0700

    category

--- NewsApp/NEWS.WEB/App/Components/admin/category/category.component.ts
+++ NewsApp/NEWS.WEB/App/Components/admin/category/category.component.ts
@@ -2,6 +2,8 @@
 import { CategoryService } from '../../../Service/category.service';
 import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 import { ICategory } from '../../../Models/category';
+import { BsModalComponent } from 'ng2-bs3-modal';
+import { DBOperation } from '../../../Shared/enum';
 //import { commondisplay } from "../../../Shared/commondisplay";
 import { Observable } from 'rxjs/Rx';
 import { Global } from '../../../Shared/global';
@@ -16,18 +18,128 @@ import { Global } from '../../../Shared/global';
 export class CategoryComponent implements OnInit {
 
     categories: ICategory[];
+    category: ICategory;
     msg: string;
     indLoading: boolean = false;
+    @ViewChild('modal') modal: BsModalComponent;
+    dbops: DBOperation;
+    categoryFrm: FormGroup;
+    modalTitle: string;
+    modalBtnTitle: string;
 
-    constructor(private _categoryService: CategoryService) { }
+    constructor(private fb: FormBuilder,private _categoryService: CategoryService) { }
 
     ngOnInit(): void {
+        this.categoryFrm = this.fb.group({
+            CategoryId: [''],
+            CategoryName: ['', Validators.required],
+            ParentId: [''],
+            Status: ['']
+        });
+
         this.LoadCategories();
     }
     LoadCategories() {
         this.indLoading = true;   
         this._categoryService.get(Global.BASE_CATEGORY_ENDPOINT)
-            .subscribe(res => { this.categories = res, console.log(res) },
+            .subscribe(res => { this.categories = res, this.indLoading = false; },
             error => this.msg = <any>error);
     }
+    addCategory() {
+        this.dbops = DBOperation.create;
+        this.SetControlsState(true);
+        this.modalTitle = "Add New Category";
+        this.modalBtnTitle = "Add";
+        this.categoryFrm.reset();
+        this.modal.open();
+    }
+
+    editCategory(id: number) {
+        this.dbops = DBOperation.update;
+        this.SetControlsState(true);
+        this.modalTitle = "Edit Category";
+        this.modalBtnTitle = "Update";
+        this.category = this.categories.filter(x => x.Id == id)[0];
+        this.categoryFrm.setValue(this.category);
+        this.modal.open();
+    }
+
+    deleteCategory(id: number) {
+        this.dbops = DBOperation.delete;
+        this.SetControlsState(false);
+        this.modalTitle = "Confirm to Delete?";
+        this.modalBtnTitle = "Delete";
+        this.category = this.categories.filter(x => x.Id == id)[0];
+        this.categoryFrm.setValue(this.category);
+        this.modal.open();
+    }
+
+    onSubmit(formData: any) {
+        this.msg = "";
+
+        switch (this.dbops) {
+            case DBOperation.create:
+                this._categoryService.post(Global.BASE_CATEGORY_ENDPOINT, formData._value).subscribe(
+                    data => {
+                        if (data == 1) //Success
+                        {
+                            this.msg = "Data successfully added.";
+                            this.LoadCategories();
+                        }
+                        else {
+                            this.msg = "There is some issue in saving records, please contact to system administrator!"
+                        }
+
+                        this.modal.dismiss();
+                    },
+                    error => {
+                        this.msg = error;
+                    }
+                );
+                break;
+            case DBOperation.update:
+                this._categoryService.put(Global.BASE_CATEGORY_ENDPOINT, formData._value.Id, formData._value).subscribe(
+                    data => {
+                        if (data == 1) //Success
+                        {
+                            this.msg = "Data successfully updated.";
+                            this.LoadCategories();
+                        }
+                        else {
+                            this.msg = "There is some issue in saving records, please contact to system administrator!"
+                        }
+
+                        this.modal.dismiss();
+                    },
+                    error => {
+                        this.msg = error;
+                    }
+                );
+                break;
+            case DBOperation.delete:
+                this._categoryService.delete(Global.BASE_CATEGORY_ENDPOINT, formData._value.Id).subscribe(
+                    data => {
+                        if (data == 1) //Success
+                        {
+                            this.msg = "Data successfully deleted.";
+                            this.LoadCategories();
+                        }
+                        else {
+                            this.msg = "There is some issue in saving records, please contact to system administrator!"
+                        }
+
+                        this.modal.dismiss();
+                    },
+                    error => {
+                        this.msg = error;
+                    }
+                );
+                break;
+
+        }
+    }
+    SetControlsState(isEnable: boolean) {
+        isEnable ? this.categoryFrm.enable() : this.categoryFrm.disable();
+    }
+
 }
