5d6fc36413a322c420adeb40c0a870d89aae84d3
xuanhung3296 <xuanhung3296@gmail.com>
2018-04-25 19:17:33 +0700

    fix

--- NewsApp/NEWS.WEB/App/Components/admin/account/account.module.ts
+++ NewsApp/NEWS.WEB/App/Components/admin/account/account.module.ts
@@ -2,12 +2,14 @@
 import { CommonModule } from "@angular/common";
 import { AccountComponent } from "./account.component";
 import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from '@angular/forms'
+import { BsModalModule } from 'ng2-bs3-modal';
 
 
 
 @NgModule({
     imports: [
-        CommonModule,
+        CommonModule, ReactiveFormsModule, BsModalModule,
         RouterModule.forChild([
             { path: '', component: AccountComponent }
         ])
