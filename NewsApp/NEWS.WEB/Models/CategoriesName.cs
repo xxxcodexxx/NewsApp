d24514be81656b6180626162c91aacbb97d63764
tdquang1196 <tdquang1196@gmail.com>
2018-06-22 01:09:19 +0700

    define categories name

---
+++ NewsApp/NEWS.WEB/Models/CategoriesName.cs
@@ -0,0 +1,9 @@
+﻿namespace NEWS.WEB.Models
+{
+    public static class CategoriesName
+    {
+        public const string THETHAO = "thethao";
+        public const string VANHOA = "vanhoa";
+        public const string CHINHTRI = "chinhtri";
+    }
+}
